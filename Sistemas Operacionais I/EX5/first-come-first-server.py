85147be324d4e7794cdca799fd7eb78086304de3
Glauber <matheus.ribeiro13@fatec.sp.gov.br>
2017-10-31 21:44:57 -0200

    Update first-come-first-server.py

--- Sistemas Operacionais I/EX5/first-come-first-server.py
+++ Sistemas Operacionais I/EX5/first-come-first-server.py
@@ -1,29 +1,24 @@
 '''FIRST COME FIRST SERVER'''
 #Simulando o funcionamento do escalonador FCFS em Python
 
-import time     #Módulo para haver controle de tempo
-
 processos = []      #Lista que conterá os processos
 
-cont = 1
+cont = int(input('\nInsira o número de processos: '))     #É solicitado o número de processos para o usuário
 
-cont = int(input('\nInsira o número de processos: '))     #É solicitado o número de
-#processos para o usuário
+for x in range(cont):       #Será realizado n ciclos, cujo n é o número de processos
+    processos.append(int(input('Processo[%d]: ' %(x+1))))       #Este ciclo irá adicionar na lista de processos os números solicitados em cada linha
 
-for x in range(cont):
-    processos.append(int(input('Processo[%d]: ' %(x+1))))
-
-temp_esp = 0
-turn = 0
-temp_esp_med = 0
-turn_med = 0
+temp_esp = 0        #Tempo de espera
+turn = 0            #Turnaround
+temp_esp_med = 0    #Tempo de espera médio
+turn_med = 0        #Tempo de turnaround médio
 
 print('\n----------------------------------------------------------------------------\nProcesso\tCiclo de CPU\tTempo de espera\t\tTempo de retorno\n----------------------------------------------------------------------------')
-for x in range(cont):
-    turn += processos[x]
-    turn_med += turn
-    print('[%d]\t\t%d\t\t%d\t\t\t%d' %(x+1, processos[x], temp_esp, turn))
-    temp_esp_med += temp_esp
+for x in range(cont):       #Será realizado n ciclos, cujo n é o número de processos
+    turn += processos[x]    #O turnaround é o tempo de execução de todo o processo, por isso é somado cada processo um com o outro
+    turn_med += turn        #O turnaround médio é o seu total dividido pelo número de processos. A divisão será realizada ao imprimir o resultado na tela
+    print('[%d]\t\t%d\t\t%d\t\t\t%d' %(x+1, processos[x], temp_esp, turn))      #É mostrado na tela os resultados
+    temp_esp_med += temp_esp        #O tempo de espera é somado depois porque o seu resultado é tratado com o início de chegada de cada processo
     temp_esp += processos[x]
 
 print('\nTempo de espera médio: %d' %(temp_esp_med/cont))
